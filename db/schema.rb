1832cfe984e14055d069d39f80912530dc5020e1
cuongtm-2892 <tran.manh.cuong@sun-asterisk.com>
2022-01-19 10:37:17 +0700

    Chapter 9: Advanced Login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_18_014928) do
+ActiveRecord::Schema.define(version: 2022_01_19_025355) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2022_01_18_014928) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
